9c74627cb85c315d6af250b173c4280ec6a75165
Daco <dacokim32@gmail.com>
2022-05-16 22:32:53 +0900

    모달창 구현, 목데이터 사용

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
   <div>
+  
+  <div class="black-bg" v-if="isShowModal">
+    <div class="white-bg">
+      <h4>상세</h4>
+      <p>상세내용</p>
+      <button @click="modalOff">창 닫기</button>
+    </div>
+  </div>
+  
   <div class="menu">
     <a v-for="(v,i) in menus" :key="i">{{ v }}</a>
   </div>
-  <div v-for="(v,k,i) in locals" :key="i">
-    <h4 :style=style>{{ k }}</h4>
-    <p>{{ v }} 만원</p>
-    <button @click="increaseReport(i)">허위매물신고</button> <span>신고수 : {{reportCount[i]}}</span>
+  
+  <div v-for="product, i in products" :key="i">
+    <img :src="product.image" class="room-img">
+    <h4 @click="modalOn">{{ product.title }}</h4>
+    <p>{{ product.price }} 원</p>
+    <button @click="increaseReport(i)">허위매물신고</button>
+    <span> 신고수 : {{reportCount[i]}}</span>
   </div>
   </div>
 </template>
 
 <script>
 
+import mockData from './assets/mockData.js';
+
 export default {
   name: 'App',
   data(){
     return {
-      reportCount : [0,0,0,0],
+      isShowModal : false,
+      reportCount : [0,0,0,0,0,0],
       menus : ["Home", "Product", "About"],
-      locals : {역삼동원룸:50, 천호동원룸:60, 마포구원룸:70, 광진구원룸:80}
+      locals : {역삼동원룸:50, 천호동원룸:60, 마포구원룸:70, 광진구원룸:80},
+      products: mockData,
     }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
   },
   methods : {
     increaseReport(i){
       this.reportCount[i]++
+    },
+    modalOn(){
+      this.isShowModal = true
+    },
+    modalOff(){
+      this.isShowModal = false
     }
   },
   components: {
@@ -33,13 +55,36 @@ export default {
 </script>
 
 <style>
+body {
+  margin : 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+
+
+.room-img {
+  width: 50%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 30px;
 }
 
 .menu {
