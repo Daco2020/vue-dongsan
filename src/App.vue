c5d4a5f2e23793b661d7b383be58d70404ba21c7
Daco <dacokim32@gmail.com>
2022-05-16 21:45:26 +0900

    클릭 이벤트 핸들러

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
   <div v-for="(v,k,i) in locals" :key="i">
     <h4 :style=style>{{ k }}</h4>
     <p>{{ v }} 만원</p>
+    <button @click="increaseReport(i)">허위매물신고</button> <span>신고수 : {{reportCount[i]}}</span>
   </div>
   </div>
 </template>
@@ -16,12 +17,18 @@ export default {
   name: 'App',
   data(){
     return {
+      reportCount : [0,0,0,0],
       menus : ["Home", "Product", "About"],
       locals : {역삼동원룸:50, 천호동원룸:60, 마포구원룸:70, 광진구원룸:80}
     }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
   },
+  methods : {
+    increaseReport(i){
+      this.reportCount[i]++
+    }
+  },
   components: {
-  }
+  },
 }
 </script>
 
